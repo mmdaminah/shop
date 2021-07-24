2b06b023838380f709b6cacf837281e107173934
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-07-24 21:37:18 +0430

    product card change a little bit

--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -1,14 +1,13 @@
 import './ProductCard.style.css'
-const ProductCard = ()=>{
+const ProductCard = () => {
     return (
-        <div className="product-card-container">
+        <div className="product-card-container rounded-3 m-5">
+            <div className="d-flex justify-content-end"><button >like</button></div>
             <img className="w-100 h-75" src="https://crdms.images.consumerreports.org/c_lfill,w_598/prod/products/cr/models/402431-smartphones-apple-iphone-12-10016496.png" alt="" />
-            <h4>title</h4>
-            <p>categoty</p>
-            <div className="d-flex">
-                <p>price</p>
-                <button>buy</button>
-            </div>
+            <div className="text-center"><h4>title</h4></div>
+            <div className="text-center">categoty</div>
+            <div className="text-center">price</div>
+            <div className="text-center"><button className="w-75 btn btn-success">buy</button></div>
         </div>
     )
 }
