90ec11295dde1dc3ed29d001c470d953c6cc6936
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-07-24 10:07:28 +0430

    added some minor changed in productSlider

--- src/Components/ProductSlider/ProductSlider.tsx
+++ src/Components/ProductSlider/ProductSlider.tsx
@@ -3,7 +3,7 @@ const ProductSlider = () => {
     const [firstItem, setFirstItem] = useState(0);
     const numberOfItemsToShow = 4;
     const itemToSlide = 1;
-    const items = ["bg-dark", "bg-success", "bg-primary", "bg-danger", "bg-warning","bg-success", "bg-primary", "bg-danger", "bg-warning"]
+    const items = ["bg-dark", "bg-success", "bg-primary", "bg-danger", "bg-warning", "bg-success", "bg-primary", "bg-danger", "bg-warning"]
     const [itemsToShow, setItemsToShow] = useState(items.slice(0, numberOfItemsToShow))
     const handleRightSlide = () => {
         if (firstItem + numberOfItemsToShow !== items.length) {
@@ -12,20 +12,23 @@ const ProductSlider = () => {
         }
     }
     const handleLeftSlide = () => {
-        if(firstItem !== 0){
+        if (firstItem !== 0) {
             setFirstItem(firstItem - itemToSlide)
-            setItemsToShow(items.slice(firstItem-itemToSlide, firstItem - itemToSlide + numberOfItemsToShow))
+            setItemsToShow(items.slice(firstItem - itemToSlide, firstItem - itemToSlide + numberOfItemsToShow))
         }
     }
     return (
-        <div className="d-flex">
-            <button onClick={handleRightSlide}>right</button>
-            {
-                itemsToShow.map((item, index) => (
-                    <div key={index + item} className={`w-25 h-25 ${item}`}>2</div>)
-                )
-            }
-            <button onClick={handleLeftSlide}>left</button>
+        <div className="d-flex flex-column">
+            <h1>title</h1>
+            <div className="d-flex">
+                <button onClick={handleRightSlide}>right</button>
+                {
+                    itemsToShow.map((item, index) => (
+                        <div key={index + item} className={`w-25 h-25 ${item}`}>2</div>)
+                    )
+                }
+                <button onClick={handleLeftSlide}>left</button>
+            </div>
         </div>
     )
 }
