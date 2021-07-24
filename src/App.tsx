2f33f4fe020b19d0f4d38bb0a658d17bbe2b96c4
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-07-24 09:31:56 +0430

    slider works well

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css'
 import MyNavbar from './Components/Navbar/Navbar';
 import MyCarousel from './Components/Carousel/Carousel';
+import ProductSlider from './Components/ProductSlider/ProductSlider';
 function App() {
   return (
     <div className="app">
       <MyNavbar />
       <div className="w-100 container">
         <MyCarousel />
+        <ProductSlider />
       </div>
     </div>
   );
