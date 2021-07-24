40dbf6e595c25b7ae125eca69e9c17c0190c49c1
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-07-24 11:04:06 +0430

    main routes initilized

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import MyNavbar from './Components/Navbar/Navbar';
 import MyCarousel from './Components/Carousel/Carousel';
 import ProductSlider from './Components/ProductSlider/ProductSlider';
 import ProductCard from './Components/ProductCard/ProductCard';
+import routes from './Routes/mainRoutes'
 function App() {
   return (
-    <div className="app">
-      <MyNavbar />
-      <div className="w-100 container">
-        <MyCarousel />
-        <ProductSlider />
-        <ProductCard />
+    <Router>
+      <div className="app">
+        <MyNavbar />
+        <div className="w-100 container">
+          <MyCarousel />
+          <ProductSlider />
+          <ProductCard />
+        </div>
       </div>
-    </div>
+      <Switch>
+        {
+          routes.map(({ path, exact, component }) => {
+            return (
+              <Route
+                path={path}
+                exact={exact}
+                render={(props) => <component {...props} />}
+              />)
+          }
+        }
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Routes/mainRoutes.ts
@@ -0,0 +1,12 @@
+const routes = [
+    {path:'/homepage',exact:true,component:"<div>homepage</div>"},
+    {path:'/login',exact:true,component:"<div>login</div>"},
+    {path:'/dashboard',exact:true,component:"<div>dashboard</div>"},
+    {path:'/category:name',exact:false,component:"<div>category</div>"},
+]
+export default routes;
+//homepage
+//login
+//product details
+//category products
+//admin dashboard
