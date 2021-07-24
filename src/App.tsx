414e0f5c05469fc85100cbe8435f6cea9dfcfd61
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-07-24 06:29:38 +0430

    top part of navbar almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import './App.css'
+import MyNavbar from './Components/Navbar/Navbar';
 function App() {
   return (
-    <></>
+    <div className="app">
+        <MyNavbar />
+    </div>
   );
 }
 
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
-const Navbar = () => {
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+const MyNavbar = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar bg="light" expand="lg" className="w-100 d-flex justify-content-between">
+            <Navbar.Brand href="#">Navbar Brand</Navbar.Brand>
+            <Form className="d-flex w-50">
+                <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="mr-2"
+                    aria-label="Search"
+                />
+            </Form>
+            <div className="d-flex">
+                <Nav.Link href="#link">login</Nav.Link>
+                <Nav.Link href="#link">wishlist</Nav.Link>
+                <Nav.Link href="#link">cart</Nav.Link>
+            </div>
+        </Navbar>
     )
 }
-export default Navbar;
+export default MyNavbar;
