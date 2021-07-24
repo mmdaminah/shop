643a4fc2a05b6bc67f05e94d05e8456b4377a33f
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-07-24 08:36:46 +0430

    slick not woriking

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css'
 import MyNavbar from './Components/Navbar/Navbar';
 import MyCarousel from './Components/Carousel/Carousel';
+import ProductSlider from './Components/ProducctSlider/ProductSlider';
 function App() {
   return (
     <div className="app">
       <MyNavbar />
       <div className="w-100 container">
         <MyCarousel />
+        <ProductSlider />
       </div>
     </div>
   );
