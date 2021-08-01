39390839a27a158e594c2cd059a38b4f97c7a527
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-08-01 17:32:59 +0430

    toast added for product adding in dashboard

--- src/Pages/Dashboard/views/AddProduct.tsx
+++ src/Pages/Dashboard/views/AddProduct.tsx
@@ -1,6 +1,8 @@
 import React, { ChangeEvent, useState } from 'react'
 import { RouteComponentProps } from 'react-router'
+import { ToastContainer, toast } from 'react-toastify'
 import IProduct from '../../../Interfaces/ProductInterface'
+import 'react-toastify/dist/ReactToastify.css';
 const AddProduct = (props: RouteComponentProps) => {
     const [input, setInput] = useState<IProduct>({
         id: "",
@@ -8,33 +10,56 @@ const AddProduct = (props: RouteComponentProps) => {
         brand: "",
         model: "",
         image: "",
-        price:""
+        price: ""
     })
     const [fileData, setFileData] = useState<Blob>(new Blob())
-    const handleFormChange = (event:React.FormEvent)=> {
+    const handleFormChange = (event: React.FormEvent) => {
         const data = event.target as HTMLInputElement
-        setInput({...input,[data.name]:data.value})
+        setInput({ ...input, [data.name]: data.value })
     }
-    const handleSubmit = (event:React.FormEvent)=>{
+    const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault()
         const data = new FormData()
-        data.append('image',fileData)
-        data.append('id',new Date().getTime().toString())
-        data.append('category',input.category)
-        data.append('brand',input.brand)
-        data.append('model',input.model)
-        data.append('price',input.price)
-        data.append('image',"")
-        fetch("/mobile",{
-            method:'post',
+        data.append('image', fileData)
+        data.append('id', new Date().getTime().toString())
+        data.append('category', input.category)
+        data.append('brand', input.brand)
+        data.append('model', input.model)
+        data.append('price', input.price)
+        data.append('image', "")
+        fetch("/mobile", {
+            method: 'post',
             // headers: {
             //     'Accept': 'application/json',
             //     'Content-Type': 'application/json'
             //   },
-            body:data
+            body: data
+        })
+        .then(()=>{
+            toast.success('کالا اضافه شد', {
+                position: "bottom-center",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+        })
+        .catch(err=>{
+            console.log(err)
+            toast.error('با مشکل مواجه شد', {
+                position: "bottom-center",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
         })
     }
-    const handleFileChange = (event:ChangeEvent)=>{
+    const handleFileChange = (event: ChangeEvent) => {
         const data = event.target as HTMLInputElement
         data.files && setFileData(data.files[0])
     }
@@ -54,6 +79,17 @@ const AddProduct = (props: RouteComponentProps) => {
                     <button className="w-25 btn btn-success">اضافه کن</button>
                 </form>
             </div>
+            <ToastContainer
+                position="bottom-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={true}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </div>
     )
 }
