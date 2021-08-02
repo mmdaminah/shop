114307e972774ef1743aea14143ed65da737c74c
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-08-02 18:43:51 +0430

    search item link added

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -10,47 +10,57 @@ import IAllProducts from '../../Interfaces/AllProducts';
 const MyNavbar = () => {
     const history = useHistory()
     const [cartShow, setCartShow] = useState(false)
+    const [inputSearch, setInputSearch] = useState("")
     const [searchItems, setSearchItems] = useState<IProduct[]>()
     const cartItems = useSelector<ICart, IProduct[]>(state => state.cart.cartProducts)
     const allProducts = useSelector<IAllProducts, IProduct[]>(state => state.allProducts.allProducts)
-    const handleSearch = (event:React.ChangeEvent) => {
+    const handleSearch = (event: React.ChangeEvent) => {
         const data = event.target as HTMLInputElement;
         console.log(data.value)
-        if(data.value === "")
+        setInputSearch(data.value)
+        if (data.value === "")
             setSearchItems([])
         else
-            setSearchItems(allProducts.filter((item)=>{
+            setSearchItems(allProducts.filter((item) => {
                 return item.model.toLowerCase().startsWith(data.value.toLowerCase())
             }))
     }
     const dispatch = useDispatch()
-    useEffect(()=>{
-        dispatch({type:"search",payload:""})
-    },[])
+    useEffect(() => {
+        dispatch({ type: "search", payload: "" })
+    }, [])
+    const handleSearchClick = (id:string, category:string)=>{
+        setInputSearch("")
+        setSearchItems([])
+        history.push(`/productdetails${id}?category=${category}`)
+    }
     return (
         <Navbar fixed="top" bg="light" expand="lg" className="w-100 d-flex flex-column">
             <div className="w-100 d-flex justify-content-between container">
                 <Navbar.Brand className="w-25" onClick={() => history.push("/homepage")} href="#">ممدشاپ</Navbar.Brand>
                 <Form className="d-flex w-50">
-                    <div className="w-100" style={{position:"relative"}}>
+                    <div className="w-100" style={{ position: "relative" }}>
                         <FormControl
                             type="search"
                             placeholder="جستجو..."
                             className="mr-2"
                             aria-label="Search"
                             onChange={handleSearch}
+                            value={inputSearch}
                         />
-                        <div className="bg-light w-100"  style={{height:"100px",position:"absolute", overflowY:"scroll"}}>
+                        <div className="bg-light w-100" style={{ height: `${searchItems ? searchItems.length * 100 > 500 ? 400 : searchItems.length * 100 : 0}px`, position: "absolute", overflowY: "scroll" }}>
                             {
-                                searchItems?.map((item)=>{
+                                searchItems?.map((item) => {
                                     return (
-                                        <div className="d-flex w-100">
-                                            <div><img style={{width:"80px",height:"80px"}} src={item.image} alt="" /></div>
-                                            <div>
-                                                <div>{item.model}</div>
-                                                <div>{item.category}</div>
+                                            <div 
+                                            onClick={()=>handleSearchClick(item.id,item.category)}
+                                            className="d-flex w-100">
+                                                <div><img style={{ width: "80px", height: "80px" }} src={item.image} alt="" /></div>
+                                                <div>
+                                                    <div>{item.model}</div>
+                                                    <div>{item.category}</div>
+                                                </div>
                                             </div>
-                                        </div>
                                     )
                                 })
                             }
