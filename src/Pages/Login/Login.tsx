1bbb31debd9ab66d1a6cc9b9bfb3407f8bd38ca6
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-08-04 08:28:11 +0430

    login page initilized

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Button, Form } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router'
-export const Login = (props:RouteComponentProps) => {
+export const Login = (props: RouteComponentProps) => {
     return (
-        <div>
-            this is Login
+        <div className="w-100 h-100" style={{ marginTop: "6rem" }}>
+            <div className="container w-100 h-75 d-flex justify-content-center align-items-center">
+                <Form className="bg-light p-5 rounded">
+                    <div className="text-center"><h1>حساب کاربری</h1></div>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>ایمیل</Form.Label>
+                        <Form.Control type="email" placeholder="examle@gmail.com" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>رمز عبور</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <div><a style={{textDecoration:"none"}} href="">فراموشی رمز عبور</a></div>
+                    <Button variant="primary" type="submit">
+                        ورود
+                    </Button>
+                </Form>
+            </div>
         </div>
     )
 }
