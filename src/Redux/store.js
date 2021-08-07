2bd69fe251380f56a7558290ff061f1a51abaa18
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-08-07 08:39:58 +0430

    redux dev tools configured

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,4 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware  } from "redux";
+import { logger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from "./RootReducer";
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,composeWithDevTools(
+    applyMiddleware(logger),
+  ));
 export default store
