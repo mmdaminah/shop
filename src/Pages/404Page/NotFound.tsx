bf0423d568d795580288fc6844b6733f2688220a
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-08-14 10:42:07 +0430

    404 page added

---
+++ src/Pages/404Page/NotFound.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import { Button } from 'react-bootstrap'
+import "./NotFound.style.css"
+import { useHistory } from 'react-router'
+const NotFound = () => {
+    const history = useHistory()
+    return (
+        <div className="w-100" style={{ marginTop: "3rem" }}>
+            <section className="page_404">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 ">
+                            <div className="col-sm-10 col-sm-offset-1  text-center">
+                                <div className="four_zero_four_bg">
+                                    <h1 className="text-center ">404</h1>
+                                </div>
+                                <div className="contant_box_404">
+                                    <h3 className="h2">
+                                    صفحه مورد نظر یافت نشد
+                                    </h3>
+                                    <p>متاسفانه صفحه‌ای که به دنبال آن بودید حذف یا منتقل شده است.</p>
+                                    <Button
+                                     onClick={()=>history.push("/homepage")}
+                                     variant="success">رفتن به صفحه اصلی</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+export default NotFound;
--- src/Routes/mainRoutes.ts
+++ src/Routes/mainRoutes.ts
@@ -6,6 +6,7 @@ const Category = React.lazy(()=>import("../Pages/Category/Category"))
 const ProductDetails = React.lazy(()=>import("../Pages/ProductDetails/ProductDetails"))
 const CartPage = React.lazy(()=>import("../Pages/CartPage/CartPage"))
 const Register = React.lazy(()=>import("../Pages/Register/Register"))
+const NotFound = React.lazy(()=>import("../Pages/404Page/NotFound"))
 const routes = [
     {path:'/homepage',exact:true,Component:HomePage,loginRequired:false},
     {path:'/login',exact:true,Component:Login,loginRequired:false},
@@ -14,5 +15,6 @@ const routes = [
     {path:'/category:name',exact:false,Component:Category,loginRequired:false},
     {path:'/productdetails:id',exact:false,Component:ProductDetails,loginRequired:false},
     {path:'/cart',exact:false,Component:CartPage,loginRequired:false},
+    {path:'*',exact:false,Component:NotFound,loginRequired:false},
 ]
 export default routes;
